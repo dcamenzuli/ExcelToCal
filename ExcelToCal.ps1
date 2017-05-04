83a361f2a940b9f2bba28cb709740dc99558c563
Dean Camenzuli <dcamenzuli@gmail.com>
2017-05-04 12:10:49 +1000

    Update ExcelToCal.ps1

--- ExcelToCal.ps1
+++ ExcelToCal.ps1
@@ -9,14 +9,14 @@ $Excel.visible = $False
 $Excel.DisplayAlerts = $False
 
 # Create a Workbook excel object, point to the correct workbook, and specify a sheet
-$Workbook = $Excel.Workbooks.Open("[WORKBOOK LOCATION]")
+$Workbook = $Excel.Workbooks.Open("[WORKBOOK LOCATION\Name]")
 $Worksheet = $Workbook.sheets.item("[SHEET NAME]")
 
 # Verify the workbook is open and display active sheets
 $Workbook.sheets | Select-Object -Property Name
 
 # Specify CSV name and location, and to save as a csv (6 = CSV)
-$Workbook.SaveAs("[CSV LOCATION]", 6)
+$Workbook.SaveAs("[CSV LOCATION\NAME.csv]", 6)
 
 # Close the excel file and kill the process
 $excel.quit()
@@ -32,12 +32,13 @@ $OutlookNamespace = $Outlook.GetNamespace("MAPI")
 
 # Set the default Outlook calendar and csv file
 $OutlookCalendar = $OutlookNamespace.GetDefaultFolder(9)
-$SourceCSV = Import-CSV "[CSV LOCATION]"
+$SourceCSV = Import-CSV "[CSV LOCATION\NAME.csv]"
 
-# create a list the same length as the CSV file
+# Create a list the same length as the CSV file
 $listCount = $SourceCSV.count
 
-# Create an array from the CSV, iterate through it adding each line item to the Calendar
+# Create an array from the CSV, iterate through it adding each line item to the Calendar 
+#The part after $SourceCSV[$i] is the title of the column you wish to grab the values from
 for($i = 0; $i -lt $listCount; ++$i)
 {
 $CalItem = $Outlook.CreateItem(1)
@@ -45,5 +46,13 @@ $CalItem.Subject = $SourceCSV[$i].subject
 $CalItem.start = $SourceCSV[$i].start
 $CalItem.end = $SourceCSV[$i].end
 $CalItem.AllDayEvent = $True
+# $CalItem.Body = $SourceCSV[$i].body
+# $CalItem.Location = $SourceCSV[$i].location
+# $CalItem.Importance = $SourceCSV[$i].importance
+# $CalItem.BusyStatus = $SourceCSV[$i].busyStatus
+# $CalItem.EnableReminder = $SourceCSV[$i].enableReminder
+# $CalItem.MeetingStart = $SourceCSV[$i].meetingStart
+# $CalItem.MeetingDuration = $SourceCSV[$i].meetingDuration
+# $CalItem.Reminder = $SourceCSV[$i].reminder
 $a = $CalItem.save()
 }
