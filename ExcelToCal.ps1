171960894cb795e9b1eca453c17eaa8f2bc9575b
Dean Camenzuli <dcamenzuli@gmail.com>
2017-05-04 16:11:49 +1000

    Update ExcelToCal.ps1

--- ExcelToCal.ps1
+++ ExcelToCal.ps1
@@ -9,14 +9,14 @@ $Excel.visible = $False
 $Excel.DisplayAlerts = $False
 
 # Create a Workbook excel object, point to the correct workbook, and specify a sheet
-$Workbook = $Excel.Workbooks.Open("[WORKBOOK LOCATION\Name]")
-$Worksheet = $Workbook.sheets.item("[SHEET NAME]")
+$Workbook = $Excel.Workbooks.Open("[CSVLocation\Name]")
+$Worksheet = $Workbook.sheets.item("[SheetName]")
 
 # Verify the workbook is open and display active sheets
 $Workbook.sheets | Select-Object -Property Name
 
 # Specify CSV name and location, and to save as a csv (6 = CSV)
-$Workbook.SaveAs("[CSV LOCATION\NAME.csv]", 6)
+$Workbook.SaveAs("[CSVLocation\Name.csv]", 6)
 
 # Close the excel file and kill the process
 $excel.quit()
@@ -30,21 +30,28 @@ $excel.quit()
 $Outlook = New-Object -ComObject Outlook.Application
 $OutlookNamespace = $Outlook.GetNamespace("MAPI")
 
-# Set the default Outlook calendar and csv file
-$OutlookCalendar = $OutlookNamespace.GetDefaultFolder(9)
-$SourceCSV = Import-CSV "[CSV LOCATION\NAME.csv]"
+# Set the default Outlook calendar to the shared Calendar
+$myRecipient = $OutlookNamespace.CreateRecipient("[SharedCalendarName]")
 
-# Create a list the same length as the CSV file
-$listCount = $SourceCSV.count
+If($myRecipient.Resolve())
+{
+	$OutlookCalendar = $OutlookNamespace.GetSharedDefaultFolder($myRecipient, 9)
+}
+
+# Set Source CSV
+$SourceCSV = Import-CSV "[CSVLocation\Name.csv]"
+
+# Create an integer to represent the size of the CSV
+$listCount = $SourceCSV.Count
 
 # Create an array from the CSV, iterate through it adding each line item to the Calendar 
 #The part after $SourceCSV[$i] is the title of the column you wish to grab the values from
 for($i = 0; $i -lt $listCount; ++$i)
 {
-	$CalItem = $Outlook.CreateItem(1)
+	$CalItem = $OutlookCalendar.Items.Add(1)
 	$CalItem.Subject = $SourceCSV[$i].subject
 	$CalItem.start = $SourceCSV[$i].start
-	$CalItem.end = $SourceCSV[$i].end
+	# $CalItem.end = $SourceCSV[$i].end
 	$CalItem.AllDayEvent = $True
 	# $CalItem.Body = $SourceCSV[$i].body
 	# $CalItem.Location = $SourceCSV[$i].location
@@ -56,3 +63,4 @@ for($i = 0; $i -lt $listCount; ++$i)
 	# $CalItem.Reminder = $SourceCSV[$i].reminder
 	$a = $CalItem.save()
 }
+
